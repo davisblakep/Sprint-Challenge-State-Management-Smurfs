20a0c04e7dc5afdacfb5f5e36ae71334863b27a2
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-06-28 19:34:24 -0400

    Added initial redux items and redux state to overall app.  State, actions, and reducers folders created.  No reducers or actions created at this time

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import 'fontsource-roboto';
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import rootReducer from './state/reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById("root"));
